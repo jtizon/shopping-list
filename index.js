bedabfc70125fe9002b52b4021d8b85bce9d8963
jtizon <47265483+jtizon@users.noreply.github.com>
2019-03-12 17:09:39 -0600

    initial commit
    Used repl.it to create code then posted to git when finished

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+function checkItems() {
+  $('.shopping-list').on('click', 
+  '.shopping-item-toggle', function(event) {
+    const targetItem = $(event.currentTarget);
+    $(targetItem).closest('li').find(".shopping-item").toggleClass('shopping-item__checked');
+  });
+}
 
+$(checkItems);
+
+function deleteItems() {
+  $('.shopping-list').on('click', 
+  '.shopping-item-delete', function(event) {
+    const targetItem = $(event.currentTarget);
+    $(targetItem).closest('li').remove();
+  });
+}
+
+$(deleteItems);
+
+function addItems(){
+  $("#js-shopping-list-form").submit(function(event) {
+    event.preventDefault();
+    const entry = $(".shopping-list-entry").val();
+    $(".shopping-list-entry").val(" ");
+    $(".shopping-list").append(
+      `<li>
+          <span class="shopping-item">${entry}</span>
+          <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>`);
+  }); 
+}
+
+$(addItems());
